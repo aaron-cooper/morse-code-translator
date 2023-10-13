d50a0d2e213f6c80dfa15203262914f8a72ad175
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2023-10-13 00:53:30 -0400

    Finish primitive functions for interracting with LCD
    will be used by lcd.c

--- lcd.s
+++ lcd.s
@@ -1,11 +1,14 @@
 RS = %001
 RW = %010
-E  = %00000100
-NE = %11111011
+E  = %100
 
 .include "zeropage.inc"
 
-.export _lcd_init
+.export _lcd_short_wait
+.export _lcd_ir_read
+.export _lcd_ir_write
+.export _lcd_dr_read
+.export _lcd_dr_write
 
 direction_a = %0110000000000011
 port_a = %0110000000000001
@@ -16,52 +19,10 @@ port_b = %0110000000000000
 
 .segment "CODE"
 
-.proc _lcd_init: near
-    lda #$ff
-    sta direction_a
-    sta direction_b
-
-    lda #$00
-    sta port_b
-
-    lda #%00111000
-    sta port_a
-    lda #E
-    sta port_b
-    lda #0
-    sta port_b
-
-    jsr lcd_short_wait
-
-    lda #%00001110
-    sta port_a
-    lda #E
-    sta port_b
-    lda #0
-    sta port_b
-
-    jsr lcd_short_wait
-
-    lda #%00000110
-    sta port_a
-    lda #E
-    sta port_b
-    lda #0
-    sta port_b
-    
-    jsr lcd_short_wait
-
-    lda #%1
-    sta port_b
-    lda #%01001000
-    sta port_a
-    lda #%101
-    sta port_b
-    lda #1
-    sta port_b
-.endproc
-
-.proc lcd_short_wait: ;wait for a minimum of 38 cycles
+;waits for 38 or more cycles to pass, this should be enough time for the LCD
+;to complete all but the "Return home" instruction. Calling this is usually
+;easier than checking the busy flag
+.proc _lcd_short_wait: near
     lda #10 ;2 cycles
 @L0: ;run loop 10 times, total of 9*3 + 2 cycles
     dec ;2 cycles
@@ -69,3 +30,62 @@ port_b = %0110000000000000
     nop
     rts ;at least 3
 .endproc
+
+;write to the LCD's instruction register. This is used for most commands
+.proc _lcd_ir_write: near
+    sta port_a
+    lda #0 ;RS = 0, RW = 0
+    sta port_b
+    jsr lcd_pulse_E
+    rts
+.endproc
+
+;write contents of A to the LCD's data register
+.proc _lcd_dr_write: near
+    sta port_a
+    lda #1 ;RS = 1, RW = 0
+    sta port_b
+    jsr lcd_pulse_E
+    rts
+.endproc
+
+; read the LCD's instruction register, return as unsigned char
+.proc _lcd_ir_read: near
+    lda #$00
+    sta direction_a
+    lda #%10 ;RW = 1, RS = 0
+    sta port_b
+    jsr lcd_pulse_E
+    lda port_a
+    sta tmp1
+    lda #$ff
+    sta direction_a
+    lda tmp1
+    ldx #0
+    rts
+.endproc
+
+; read the LCD's data register, return as unsigned char
+.proc _lcd_dr_read: near
+    lda #$00
+    sta direction_a
+    lda #%11 ;RW = 1, RS = 1
+    sta port_b
+    jsr lcd_pulse_E
+    lda port_a
+    sta tmp1
+    lda #$ff
+    sta direction_a
+    lda tmp1
+    ldx #0
+    rts
+.endproc
+
+; the contents of A (aside from bit 2 which corresponds to E) will be written to
+; port_b as is
+lcd_pulse_E:
+    ora #%100
+    sta port_b
+    and #%11111011
+    sta port_b
+    rts
