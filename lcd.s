edd7cd52e95b79bf86b66c1ff1f6888886891522
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2023-10-13 00:01:25 -0400

    Add 38+ cycle wait between initialization instructions

--- lcd.s
+++ lcd.s
@@ -24,12 +24,14 @@ port_b = %0110000000000000
     lda #$00
     sta port_b
 
-    lda #%00110000
+    lda #%00111000
     sta port_a
     lda #E
     sta port_b
     lda #0
     sta port_b
+
+    jsr lcd_short_wait
 
     lda #%00001110
     sta port_a
@@ -38,12 +40,16 @@ port_b = %0110000000000000
     lda #0
     sta port_b
 
+    jsr lcd_short_wait
+
     lda #%00000110
     sta port_a
     lda #E
     sta port_b
     lda #0
     sta port_b
+    
+    jsr lcd_short_wait
 
     lda #%1
     sta port_b
@@ -53,4 +59,13 @@ port_b = %0110000000000000
     sta port_b
     lda #1
     sta port_b
+.endproc
+
+.proc lcd_short_wait: ;wait for a minimum of 38 cycles
+    lda #10 ;2 cycles
+@L0: ;run loop 10 times, total of 9*3 + 2 cycles
+    dec ;2 cycles
+    bne @L0 ;3 except for final, which is 2
+    nop
+    rts ;at least 3
 .endproc
